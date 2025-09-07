27de7eeadb3c27a29e7bebc352281b505b5c1238
Mauricio-Or-A <m.ortega@udea.edu.co>
2025-09-06 22:41:04 -0500

    Update Jugador.java

--- src/Jugador.java
+++ src/Jugador.java
@@ -107,5 +107,112 @@ public class Jugador {
                 ? "No se encontraron escaleras de la misma pinta"
                 : resultado.toString();
     }
+    public int Puntaje() {
+        //antes tomamos el enfoque de sacar el puntaje total y luego restarle los puntajes de las parejas y escaleras//
+        //pero nos dimos cuenta de el enfoque que ver cuales cartas no formaban parte de una pareja o escalera era mas facil//
+        int puntaje = 0;
+        int residuo;
+        int i=0;
+        int[] contadores1 = new int[NombreCarta.values().length];
+        int[][] valoresCartas=new int[TOTAL_CARTAS][2];
+        ArrayList<Carta> cartasParejas= new ArrayList<>();
+        ArrayList<Carta> cartasEscalera= new ArrayList<>();
+        ArrayList<Carta> cartasIndividuales= new ArrayList<>();
 
+        for (Carta c : cartas) {
+            residuo = c.getIndice() % 13;
+            if (residuo == 0) {
+                residuo = 13;
+            }
+            valoresCartas[i][0]=residuo;
+            valoresCartas[i][1]=c.getPinta().ordinal();
+            i += 1;
+        }
+        //sacamos las cartas que estan en las parejas//
+        for (Carta c : cartas) {
+            contadores1[c.getNombre().ordinal()]++;
+            if (contadores1[c.getNombre().ordinal()] >= 2) {
+                for (Carta carta : cartas) {
+                    if (carta.getNombre().ordinal() == c.getNombre().ordinal() && !cartasParejas.contains(carta)) {
+                        cartasParejas.add(carta);
+                    }
+                }
+            }
+        }
+        for (Carta c : cartasParejas) {
+            System.out.println(c.getNombre()+" "+c.getPinta());
+        }
+        //vemos que cartas estan en escalera//
+        for (i = 0; i < valoresCartas.length; i++) {
+            for (int j = 0; j < valoresCartas.length - 1; j++) {
+                if (valoresCartas[j][1] > valoresCartas[j + 1][1] || (valoresCartas[j][1] == valoresCartas[j + 1][1]
+                        && valoresCartas[j][0] > valoresCartas[j + 1][0])) {
+                    int numeroTemporal = valoresCartas[j + 1][0];
+                    int pintaTemporal = valoresCartas[j + 1][1];
+                    valoresCartas[j + 1][0] = valoresCartas[j][0];
+                    valoresCartas[j][0] = numeroTemporal;
+                    valoresCartas[j + 1][1] = valoresCartas[j][1];
+                    valoresCartas[j][1] = pintaTemporal;
+                    
+                }
+            }
+        }
+        for (i = 0; i < valoresCartas.length; i++) {
+            int longitudEscalera = 1;
+            ArrayList<Carta> escaleraTemporal = new ArrayList<>();
+            for (Carta carta : cartas) {
+                if (carta.getIndice() % 13 == valoresCartas[i][0] && carta.getPinta().ordinal() == valoresCartas[i][1]
+                        && !cartasEscalera.contains(carta)) {
+                    escaleraTemporal.add(carta);
+                    break;
+                }
+            }
+            for (int j = i; j < valoresCartas.length - 1; j++) {
+                if (valoresCartas[j][0] + 1 == valoresCartas[j + 1][0] && valoresCartas[j][1] == valoresCartas[j + 1][1]) {
+                    longitudEscalera++;
+                    for (Carta carta : cartas) {
+                        if (carta.getIndice() % 13 == valoresCartas[j][0] + 1 && carta.getPinta().ordinal() == valoresCartas[j][1]
+                            && !cartasEscalera.contains(carta)) {
+                            escaleraTemporal.add(carta);
+                        }
+                    }
+                } else {
+                    break; 
+                }
+            }
+            if (longitudEscalera > 1) {
+                cartasEscalera.addAll(escaleraTemporal);
+                i += longitudEscalera - 1; 
+            }
+        }
+        for (Carta c : cartasEscalera) {
+            System.out.println(c.getNombre()+" "+c.getPinta());
+        }
+        for (Carta carta : cartas) {
+            if (!cartasEscalera.contains(carta)&&!cartasParejas.contains(carta)){
+                cartasIndividuales.add(carta);
+            }
+        }
+        System.out.println("Cartas individuales");
+        for (Carta c : cartasIndividuales) {
+            System.out.println(c.getNombre()+" "+c.getPinta());
+        }
+
+
+        int valorCartasIndividuales = 0;
+        for (Carta c : cartasIndividuales) {
+            int valorCarta = c.getIndice() % 13;
+            if (valorCarta==0){
+                valorCarta=13;
+            }
+            if (valorCarta == 1 || valorCarta >= 11) {
+                valorCartasIndividuales += 10;
+            } else {
+                valorCartasIndividuales += valorCarta;
+            }
+        }
+        
+        puntaje=valorCartasIndividuales;
+        return puntaje;
+    }
 }
